4fdfe3bdd6b1928bea480fdb18a3862d5e1dea97
unknown <risnarendra@gmail.com>
2020-04-15 16:21:50 +0530

    Updates

---
+++ precache-manifest.4db4704001c58d666d690a14a77cf9ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a265f051fa9cb3bee105c429cad8ab55",
+    "url": "/Covid-19Tracker/index.html"
+  },
+  {
+    "revision": "885a42d0e9224c473291",
+    "url": "/Covid-19Tracker/static/css/main.48cd731a.chunk.css"
+  },
+  {
+    "revision": "d7badfdbe7f3bfa5d168",
+    "url": "/Covid-19Tracker/static/js/2.99c323be.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19Tracker/static/js/2.99c323be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "885a42d0e9224c473291",
+    "url": "/Covid-19Tracker/static/js/main.37fe81ab.chunk.js"
+  },
+  {
+    "revision": "725e09895f7af105a0d4",
+    "url": "/Covid-19Tracker/static/js/runtime-main.5442c6a2.js"
+  }
+]);
